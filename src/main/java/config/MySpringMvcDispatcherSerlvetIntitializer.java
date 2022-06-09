e1a769cc90ebceafb2c77cf76b7b702db93f21f7
MinibaevaElvira <MinibaevaElvirar@gmail.com>
2022-06-09 13:19:06 +0300

    pp 2.3.1

---
+++ src/main/java/config/MySpringMvcDispatcherSerlvetIntitializer.java
@@ -0,0 +1,22 @@
+package config;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+public class MySpringMvcDispatcherSerlvetIntitializer extends AbstractAnnotationConfigDispatcherServletInitializer{
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[0];
+    }
+}
