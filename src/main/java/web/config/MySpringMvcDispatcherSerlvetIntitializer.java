466d622d2209b3baf27a3846e2f23c2e1aaa13e8
MinibaevaElvira <MinibaevaElvirar@gmail.com>
2022-06-09 18:38:38 +0300

    1

--- src/main/java/web/config/MySpringMvcDispatcherSerlvetIntitializer.java
+++ src/main/java/web/config/MySpringMvcDispatcherSerlvetIntitializer.java
@@ -1,22 +1,19 @@
-package config;
-import org.springframework.web.filter.HiddenHttpMethodFilter;
+package web.config;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
 public class MySpringMvcDispatcherSerlvetIntitializer extends AbstractAnnotationConfigDispatcherServletInitializer{
     @Override
     protected Class<?>[] getRootConfigClasses() {
-        return new Class[0];
+        return null;
     }
 
     @Override
     protected Class<?>[] getServletConfigClasses() {
-        return new Class[0];
+        return new Class[]{WebConfig.class};
     }
 
     @Override
     protected String[] getServletMappings() {
-        return new String[0];
+        return new String[]{"/"};
     }
 }
--- src/main/java/web/model/User.java
+++ src/main/java/web/model/User.java
@@ -1,6 +1,12 @@
-package model;
+package web.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
     private String surname;
